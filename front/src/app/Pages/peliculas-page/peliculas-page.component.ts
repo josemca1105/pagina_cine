63e59b527e28545bc661e577295434e1af596097
josemca1105 <jm.calderon1105@gmail.com>
2024-02-11 15:31:00 -0400

    tabla pelis (sin imagen)

--- front/src/app/Pages/peliculas-page/peliculas-page.component.ts
+++ front/src/app/Pages/peliculas-page/peliculas-page.component.ts
@@ -1,12 +1,44 @@
+import { NgFor, CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-peliculas-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgxPaginationModule, CommonModule],
   templateUrl: './peliculas-page.component.html',
   styleUrl: './peliculas-page.component.css'
 })
 export class PeliculasPageComponent {
+  p: number = 1;
 
+  imagen: string = '';
+  nombre: string = '';
+  descripcion: string = '';
+  duracion: string = '';
+  genero: string = '';
+  estreno: string = '';
+
+  currentPeliculaID = '';
+  peliculaArray: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.getAllPelicula();
+  }
+
+  getAllPelicula() {
+    this.http.get("http://127.0.0.1:8000/api/peliculas").subscribe((resultData: any)=> {
+        // console.log(resultData);
+        this.peliculaArray = resultData;
+    });
+  }
+
+  setDelete(data: any) {
+    this.http.delete("http://127.0.0.1:8000/api/peliculas/delete" + "/" + data.id).subscribe((resultData: any) => {
+        // console.log(resultData);
+        alert("Pelicula Eliminado")
+        this.getAllPelicula();
+    });
+  }
 }
